576884294d1a30178b3d3a049b846be8271acab3
Bernardo Alvarez Braga <bernardo-braga@bol.com.br>
2019-01-26 10:32:01 -0200

    Movimento melhorado

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,26 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Helpers.Routine;
+using Helpers;
 
 public class movement : MonoBehaviour
 {
+    public float strength = 2.0f;
     float speed;
     [SerializeField]
     Sprite hexagon;
 
-    bool canMove;
+    bool hasMoved = false;
     float moveHorizontal;
     float moveVertical;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         speed = 0.2f;
-        canMove = true;
-        //Debug.Log("Hexagon: " + hexagon.bounds.size.x);
+        hasMoved = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         move();
@@ -32,51 +33,39 @@ public class movement : MonoBehaviour
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetAxis("Vertical");
 
-        if (moveHorizontal != 0 || moveVertical != 0)
-        {
-            Debug.Log("moveHorizontal: " + moveHorizontal);
-            StartCoroutine("moveTo");
-        }
+        Vector3 dir = new Vector3(moveHorizontal, moveVertical, 0f).normalized;
 
-        //gameObject.transform.Translate(new Vector2(transform.position.x + moveHorizontal * speed * Time.deltaTime, transform.position.y));
-    }
+        //if (!hasMoved && (Mathf.Abs(moveHorizontal) > 0.1f || (Mathf.Abs(moveVertical) > 0.1f && Mathf.Abs(moveHorizontal) > 0.1f)))
+        //{
+            //if (moveHorizontal > 0f)
+            //{
+            //    // direita cima
+            //    if (moveVertical > 0f)
+            //        dir = new Vector3(1.0f, 1.0f, 0.0f).normalized;
+            //    // direita baixo
+            //    if (moveVertical < 0f)
+            //        dir = new Vector3(1.0f, -1.0f, 0.0f).normalized;
+            //    // direita
+            //    if (Mathf.Abs(moveVertical) < 0.1f)
+            //        dir = new Vector3(1.0f, 0.0f, 0.0f).normalized;
+            //}
+            //else
+            //{
+            //    // direita cima
+            //    if (moveVertical > 0f)
+            //        dir = new Vector3(-1.0f, 1.0f, 0.0f).normalized;
+            //    // direita baixo
+            //    if (moveVertical < 0f)
+            //        dir = new Vector3(-1.0f, -1.0f, 0.0f).normalized;
+            //    // direita
+            //    if (Mathf.Abs(moveVertical) < 0.1f)
+            //        dir = new Vector3(-1.0f, 0.0f, 0.0f).normalized;
+            //}
 
-    IEnumerator moveTo()
-    {
-        if (canMove)
-        {
-            if(Input.GetAxis("Horizontal") > 0.0f)
-            {
-                //gameObject.transform.Translate(new Vector2(transform.position.x + hexagon.bounds.size.x, transform.position.y));
-                transform.position += new Vector3(hexagon.bounds.size.x, 0, 0);
-                Debug.Log("DIREITA");
-            }
-            else if(Input.GetAxis("Horizontal") < 0.0f)
-            {
-                //gameObject.transform.Translate(new Vector2(transform.position.x - hexagon.bounds.size.x, transform.position.y));
-                transform.position -= new Vector3(hexagon.bounds.size.x, 0, 0);
-                Debug.Log("ESQUERDA");
-            }
-
-            if (Input.GetAxis("Vertical") > 0.0f)
-            {
-                //gameObject.transform.Translate(new Vector2(transform.position.x, transform.position.y + hexagon.bounds.size.y));
-                transform.position += new Vector3(0, hexagon.bounds.size.y, 0);
-                Debug.Log("CIMA");
-            }
-            else if (Input.GetAxis("Vertical") < 0.0f)
-            {
-                //gameObject.transform.Translate(new Vector2(transform.position.x, transform.position.y - hexagon.bounds.size.y));
-                transform.position -= new Vector3(0, hexagon.bounds.size.y, 0);
-                Debug.Log("BAIXO");
-            }
-
-        }
-
-        canMove = false;
-
-        yield return new WaitForSeconds(1.0f);
-
-        canMove = true;
+            Debug.Log(dir * strength);
+            GetComponent<Rigidbody2D>().AddForce(dir * strength, ForceMode2D.Force);
+            hasMoved = true;
+            //ActionHelper.Delay(1f, () => { hasMoved = false; });
+        //}
     }
 }
