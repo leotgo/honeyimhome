266553cb3b3847c5c362470dbe9127ea36c61417
SaintBallista <jmaniquehenz@gmail.com>
2019-01-26 21:23:15 -0200

    adicionado alguns código de pickup

---
+++ Assets/Scripts/Tiles/WaxTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaxTile : HexagonTile
+{
+    public GameObject pickupPrefab;
+    public float spawnTime = 5f;
+    private float currentTime = 0f;
+
+    private void Start()
+    {
+        currentTime = 0f;
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > spawnTime)
+        {
+            SpawnPickup();
+            currentTime = 0f;
+        }
+    }
+
+    private void SpawnPickup()
+    {
+        var pickup = Instantiate(pickupPrefab);
+        pickup.transform.position = transform.position;
+    }
+}
