75488802c06bd0f86e2358b19e3190fe7d5186d3
Bernardo Alvarez Braga <bernardo-braga@bol.com.br>
2019-01-26 13:56:27 -0200

    Entidades

---
+++ Assets/Scripts/EntitiesInteractions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitiesInteractions
+{
+    public struct Interaction
+    {
+        public entitiesTypes entity1;
+        public entitiesTypes entity2;
+        public entitiesTypes result;
+    }
+
+    // A Key do dicionário (KeyValuePair) representa os dois elementos da interação
+    // O Value do dicionário representa o resultado da interação entre esses dois elementos
+    public List<Interaction> interactionResults; 
+    
+    
+}
