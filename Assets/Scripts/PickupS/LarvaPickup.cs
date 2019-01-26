ea6e461a8423b111019df79b0a2c1d1cab110662
SaintBallista <jmaniquehenz@gmail.com>
2019-01-26 20:40:12 -0200

    código para os pickups

---
+++ Assets/Scripts/PickupS/LarvaPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LarvaPickup : pickup
+{
+    public override void OnInteractWithPickup(pickup other)
+    {
+        switch (other.type)
+        {
+            case PickupType.Honey:
+                CombineToNewEntity(other, EntityPrefabHolder.instance.beePrefab);
+                break;
+            case PickupType.Wax:
+                CombineToNewEntity(other, PickupPrefabHolder.instance.larvaPickupPrefab);
+                break;
+            case PickupType.Polen:
+                CombineToNewEntity(other, PickupPrefabHolder.instance.waxPickupPrefab);
+                break;
+        }
+    }
+
+    public override void OnInteractWithTile(HexagonTile tile)
+    {
+
+    }
+
+}
