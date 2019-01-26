1f6325b926dbf51f6b307362499d7a99926c9cbe
Bernardo Alvarez Braga <bernardo-braga@bol.com.br>
2019-01-26 02:41:50 -0200

    Movimento quase funcionando

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -8,11 +8,16 @@ public class movement : MonoBehaviour
     [SerializeField]
     Sprite hexagon;
 
+    bool canMove;
+    float moveHorizontal;
+    float moveVertical;
+
     // Start is called before the first frame update
     void Start()
     {
         speed = 0.2f;
-        Debug.Log("Hexagon: " + hexagon.bounds.size.x);
+        canMove = true;
+        //Debug.Log("Hexagon: " + hexagon.bounds.size.x);
     }
 
     // Update is called once per frame
@@ -24,15 +29,54 @@ public class movement : MonoBehaviour
 
     void move()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
 
         if (moveHorizontal != 0 || moveVertical != 0)
         {
-            gameObject.transform.Translate(new Vector2(transform.position.x + hexagon.bounds.size.x * Time.deltaTime, transform.position.y));
-            Debug.Log(moveHorizontal);
+            Debug.Log("moveHorizontal: " + moveHorizontal);
+            StartCoroutine("moveTo");
         }
 
         //gameObject.transform.Translate(new Vector2(transform.position.x + moveHorizontal * speed * Time.deltaTime, transform.position.y));
+    }
+
+    IEnumerator moveTo()
+    {
+        if (canMove)
+        {
+            if(Input.GetAxis("Horizontal") > 0.0f)
+            {
+                //gameObject.transform.Translate(new Vector2(transform.position.x + hexagon.bounds.size.x, transform.position.y));
+                transform.position += new Vector3(hexagon.bounds.size.x, 0, 0);
+                Debug.Log("DIREITA");
+            }
+            else if(Input.GetAxis("Horizontal") < 0.0f)
+            {
+                //gameObject.transform.Translate(new Vector2(transform.position.x - hexagon.bounds.size.x, transform.position.y));
+                transform.position -= new Vector3(hexagon.bounds.size.x, 0, 0);
+                Debug.Log("ESQUERDA");
+            }
+
+            if (Input.GetAxis("Vertical") > 0.0f)
+            {
+                //gameObject.transform.Translate(new Vector2(transform.position.x, transform.position.y + hexagon.bounds.size.y));
+                transform.position += new Vector3(0, hexagon.bounds.size.y, 0);
+                Debug.Log("CIMA");
+            }
+            else if (Input.GetAxis("Vertical") < 0.0f)
+            {
+                //gameObject.transform.Translate(new Vector2(transform.position.x, transform.position.y - hexagon.bounds.size.y));
+                transform.position -= new Vector3(0, hexagon.bounds.size.y, 0);
+                Debug.Log("BAIXO");
+            }
+
+        }
+
+        canMove = false;
+
+        yield return new WaitForSeconds(1.0f);
+
+        canMove = true;
     }
 }
