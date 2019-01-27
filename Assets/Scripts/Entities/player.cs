50117f63a10d5354c58c233eb2b5a016fe3efe66
Bernardo Alvarez Braga <bernardo-braga@bol.com.br>
2019-01-26 22:06:24 -0200

    Arte inclusa

--- Assets/Scripts/Entities/player.cs
+++ Assets/Scripts/Entities/player.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class player : Bee
 {
+
+    [SerializeField]
+    private Sprite[] playerSprites;
+    private int currFrameIndex = 0;
+    private float animTime = 0.04f;
+    private float currTime = 0f;
+
+    private void Start()
+    {
+        currFrameIndex = 0;
+        currTime = 0f;
+    }
+
+    private void Update()
+    {
+        currTime += Time.deltaTime;
+        if(currTime > animTime)
+        {
+            currTime = 0;
+            currFrameIndex = (currFrameIndex == playerSprites.Length - 1) ? 0 : currFrameIndex + 1;
+            GetComponent<SpriteRenderer>().sprite = playerSprites[currFrameIndex];
+        }
+    }
+
     public void spawnTile()
     {
         if(carryingObject != null)
